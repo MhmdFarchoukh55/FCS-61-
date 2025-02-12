85dca367caa90ae304cd3605e073dca04dd2fa25
MhmdFarchoukh55 <mohammad.farchoukh55@gmail.com>
2025-02-12 21:58:16 +0200

    assigment_1

---
+++ ass_1.py.py
@@ -0,0 +1,24 @@
+#ex1
+age=input("Enter your age: ")
+
+
+
+
+#ex2
+number=int(input("Enter a number: "))
+if number%2==0:
+    print("The number",number,"is even.")
+else:
+    print("The number",number, "is odd.")
+
+#ex3
+#If the username is "admin" and the password is "1234", print "Access granted".
+username=input("enter username")
+password=input("enter ur password")
+if username=="admin":
+    if password=="1234":
+        print("Access granted")
+    else:
+        print("Access denied")
+else:
+    print("Access denied")
